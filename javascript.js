f89d0f74be2e17445300f665f06c573c2fb691bd
BigDaveCoding <bigdavecoding@gmail.com>
2024-12-13 11:04:40 +0000

    working on animating the navigation list. Also added animation to my name and title in which the text types in on page load. I think i want to take this further and have the text write itself in and then trigger some fade in animations on other elemens on the page such as the photos or the about me text

--- javascript.js
+++ javascript.js
@@ -1,23 +1,49 @@
 document.addEventListener("DOMContentLoaded", (event) => {
-    gsap.registerPlugin(ScrollTrigger,TextPlugin)
+    gsap.registerPlugin(ScrollTrigger, TextPlugin)
 
     console.log(gsap);
 
-    const navLinks = document.querySelectorAll(".nav_link_link");
-
+    navLinks = document.querySelectorAll('.nav_link_link');
+    // animation for navigation links
     navLinks.forEach((link) => {
-        link.addEventListener("mouseover", () => {
-            console.log("hovered");
+        link.addEventListener('mouseover', () => {
+            console.log('hovering over link');
             gsap.to(link, {
-                scale: 1.1,
+                y: -2.5,
                 duration: 0.2,
-                ease: "power1.out"
+                stagger: 0.05,
+                opacity: 1,
+                ease: 'power2.inOut'
             });
         });
-        link.addEventListener("mouseleave", () => {
-            gsap.to(link, { scale: 1, duration: 0.3, ease: "power1.out" });
-        });
+
+        link.addEventListener('mouseleave', () => {
+            console.log('leaving link');
+            gsap.to(link, {
+                y: 0,
+                duration: 0.2,
+                stagger: 0.05,
+                opacity: 0.8,
+                ease: 'power2.inOut'
+            }); 
+        })
     });
 
+    //animation for name and title
+    myName = document.querySelector('#name');
+    myTitle = document.querySelector('#title');
+    console.log(myName);
+
+    gsap.to(myName, {
+        text: 'DAVID SMITH',
+        duration: 2,
+        ease: 'power2.inOut'
+    });
+
+    gsap.to(myTitle, {
+        text: 'SOFTWARE DEVELOPER',
+        duration: 3,
+        ease: 'power2.inOut'
+    });
 });
 
